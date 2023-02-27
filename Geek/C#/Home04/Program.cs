8256f3b09ee9c1bad14400d787938e8ac67a1c9b
Bugaev Kirill <DurdenRD@gmail.com>
2023-02-28 01:18:54 +0300

    Решил задачу 4

---
+++ Geek/C#/Home04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+
+Console.Write("Введите первое целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число с: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max)
+{
+    max=b;
+}
+
+    if (c>max)
+    {
+        max=c;
+    }
+    
+            Console.WriteLine($"Максимальное число равно: {max}");
