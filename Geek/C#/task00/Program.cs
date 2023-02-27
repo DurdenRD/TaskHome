4d567ced6d0b2d8dd82453a9b113cd31ccb5b707
Bugaev Kirill <DurdenRD@gmail.com>
2023-02-27 22:36:25 +0300

    решили задачу 0

--- Geek/C#/task00/Program.cs
+++ Geek/C#/task00/Program.cs
@@ -9,5 +9,6 @@
 Console.WriteLine("Введите целое число:");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
+
 Console.WriteLine($"Квадрат числа {number} = {square}");
 
