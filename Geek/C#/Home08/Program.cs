774cdc884042cb1a2829db6c600c67094f5e5ec2
Bugaev Kirill <DurdenRD@gmail.com>
2023-02-28 02:12:09 +0300

    Решил все задачи

---
+++ Geek/C#/Home08/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, 
+// которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Введите целое число N больше 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>1)
+{   
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Все четные числа от 1 до числа N = ");
+    for (int i = 2; n>=i; i=i+2)
+{
+    Console.Write($"{i} "); 
+}
+
+}
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Не верное число, попробуй еще раз умник!");
+}
