87a00f578e1514a49831536ecaf0553bddb281c1
Bugaev Kirill <DurdenRD@gmail.com>
2023-02-28 00:57:06 +0300

    решил задачу 2

--- Geek/C#/Home02/Program.cs
+++ Geek/C#/Home02/Program.cs
@@ -2,5 +2,29 @@
 //  которая на вход принимает два числа и выдаёт, 
 //  какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите первое целое число:");
-int a = ConConsole.ReadLine()
+Console.Write("Введите первое целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+
+    Console.WriteLine("Числа равные, попробуйте еще раз!");
+
+}
+else
+{
+    if (a > b)
+    {
+        Console.WriteLine($"Число a={a} - большее число, а Число b={b} - меньшее число.");
+    }
+    else
+    {
+        Console.WriteLine($"Число b={b} - большее число, а Число a={a} - меньшее число.");
+
+    }
+}
+
+
