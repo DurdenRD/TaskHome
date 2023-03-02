502884287964adfaabac177795a27410955e91c4
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-03 01:35:06 +0300

    add task11

---
+++ Geek/C#/task11/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int FirstThird(int n) 
+{
+    int c = n % 10; //первая цифра цифра
+    int a = n/100; //третья цифра
+    int x = c+10*a;
+    return x;
+    
+}
+
+//int number = new Random().Next(100, 1000);
+int number = 100;
+Console.WriteLine(number);
+
+int result = FirstThird(number);
+Console.WriteLine(result);
+
+
+
