164043bc4e2c5254a26b306c80ef0fad26c3f8f2
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-04 04:05:35 +0300

    Home task10

---
+++ Geek/C#/task 10/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+
+int Sec(int n) 
+{
+    int c = n / 10; // вторая + третья
+    int b = c % 10; //вторая цифра
+   
+    return b;
+    
+}
+
+bool True(int num)
+{
+    return num > 99 && num <1000 || num < -99 && num > -1000;
+}
+
+
+Console.WriteLine("Введите трех значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Sec(number);
+
+if (result<0)
+ {
+    result = -result;
+}
+Console.WriteLine(True (number) ?  $"{number}->{result}" : "Это не трехзначное число, досвидания умник!"); 
+
---
+++ Geek/C#/task 16 bool/Program.cs
@@ -0,0 +1,32 @@
+﻿// 16. Напишите программу, которая принимает 
+// на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+  
+// 5, 25 -> да 
+// -4, 16 -> да 
+// 25, 5 -> да 
+// 8,9 -> нет
+
+
+
+
+
+
+
+bool Sqr(int numb1, int numb2)
+{
+    return numb1 == numb2 * numb2 || numb2 == numb1 * numb1 ; 
+}
+
+Console.WriteLine("input first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sqr(a,b) ? "да" : "нет");
+
+//Sqr (a1,b1) ? Console.WriteLine($"{a}, {b} -> да") : Console.WriteLine($"{a}, {b} -> нет");
+
+
+
+
