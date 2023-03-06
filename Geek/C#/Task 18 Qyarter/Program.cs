91a9d23e2c0b33d10e876adcbf8975ee11e8ee2a
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-07 01:07:00 +0300

    add task18 quarter and rang

---
+++ Geek/C#/Task 18 Qyarter/Program.cs
@@ -0,0 +1,25 @@
+﻿//По заданому номеру четверти, показывает диапозон точек (x, y)
+
+// int Quarter(int x, int y)
+// {
+//     if (x>0 && y>0) return 1;
+//     if (x<0 && y>0) return 2;
+//     if (x<0 && y<0) return 3;
+//     if (x>0 && y<0) return 4;
+//     return 0;
+       
+// }
+string Rang (int index)
+{
+    if (index == 1) return "x>0, y>0";
+    if (index == 2) return "x<0, y>0";
+    if (index == 3) return "x<0, y<0";
+    if (index == 4) return "x>0, y<0";
+    return "не верная четверть!";
+       
+}
+
+Console.WriteLine("Введите номер четверти:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string result = Rang(quarter);
+Console.WriteLine (result);
