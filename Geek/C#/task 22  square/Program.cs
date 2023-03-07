941a5bfaeb0cc5bb792b192ff623d782b3ade70c
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-07 21:32:24 +0300

    Task 22 tablet square

---
+++ Geek/C#/task 22  square/Program.cs
@@ -0,0 +1,27 @@
+﻿//Квадратов вывод чисел от 1 до N 
+double Square(int a)
+{
+    double x = Math.Pow(a, 2);
+    return x;
+}
+
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i < N; i++)
+
+// {
+//     Console.Write($"{Square(i)}, ");
+// }
+
+
+// Console.WriteLine($"{Square(N)}.");
+
+int i = 1;
+while (i <=N )
+{
+    Console.WriteLine($"{i}  {Square(i)}");
+    i++;
+}
+
