db45cadd315c68bac6822e9558269fd4121a1011
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-12 00:35:20 +0300

    task 28 factorial

---
+++ Geek/C#/task 28/Program.cs
@@ -0,0 +1,30 @@
+﻿//от 1 до N - произведения
+
+
+int Product(int x)
+
+{
+    int n = 2;
+    int productNumber = 1;
+
+    checked
+    {
+        do
+        {
+            productNumber *= n;
+            n++;
+
+        }
+        while (n <= x);
+    }
+    return productNumber;
+
+}
+
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int result = Product(N);
+Console.WriteLine($"Произведение чисел от 1 до {N} = {result}");
