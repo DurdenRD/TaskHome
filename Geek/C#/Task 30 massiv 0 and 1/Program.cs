1bba2ca847c02a67fd76b4bad688c467a2b2261a
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-12 10:37:29 +0300

    add task 30 massiv 8 - 0 and 1

---
+++ Geek/C#/Task 30 massiv 0 and 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// massive 8 lenthg random 0 and 1
+
+// int [] array = new int[8];
+// array[i] = new Random().Next(0,1);
+// Console.WriteLine(array);
+
+void Massive(int[] arr)
+{
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(0,2);
+        }
+    
+}
+
+void Printarray(int[]arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+int [] array = new int[8];
+
+Massive(array);
+Printarray(array);
