7c45100f1b5f6fd5c5f4647667caeef0643f2cf5
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-12 16:28:06 +0300

    add home 29

---
+++ Geek/C#/Home 29 Massiv 8 rundom numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int [] array = new int[8];
+Array(array);
+Printarray(array);
+Console.Write($"{array[array.Length-1]}");
+
+void Array(int [] arr)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+     array[i] = random.Next(-99,100);   
+    }
+}
+
+void Printarray(int [] arr)
+
+{
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    
+
+}
