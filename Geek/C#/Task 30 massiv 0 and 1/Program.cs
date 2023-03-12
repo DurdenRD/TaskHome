eb238ba2dc683f2b03132f9a7632698e8f63306a
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-12 10:50:41 +0300

    add task 30 new function Random

--- Geek/C#/Task 30 massiv 0 and 1/Program.cs
+++ Geek/C#/Task 30 massiv 0 and 1/Program.cs
@@ -6,10 +6,10 @@
 
 void Massive(int[] arr)
 {
-
+    Random rnd = new Random();
         for (int i = 0; i < arr.Length; i++)
         {
-            arr[i] = new Random().Next(0,2);
+            arr[i] = rnd.Next(0,2);
         }
     
 }
