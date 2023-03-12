5729a6e2847738a01d6e9a59a55b16624a6b3649
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-13 01:20:21 +0300

    add task 32

---
+++ Geek/C#/Task 32 Massive sign/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreatArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[arr.Length - 1]}");
+
+    }
+    Console.WriteLine("]");
+
+}
+
+int[] Sign(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+    
+}
+
+int[] array = CreatArray(5, -10, 11);
+PrintArray(array);
+int[] revers = Sign(array);
+PrintArray(revers);
+
