ca658695e1439f420daa4036857a7f5984085fc7
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-15 16:38:17 +0300

    add home 34, 36

---
+++ Geek/C#/36 Sum of odd position/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int[] array = CreatArray(5, -100, 100);
+
+int result = SummNumber(array);
+
+PrintArray(array);
+Console.Write($" -> {result}");
+
+int[] CreatArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[arr.Length - 1]}]");
+    }
+
+
+}
+
+int SummNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (i % 2 == 1) count += arr[i];
+    }
+    return count;
+}
---
+++ Geek/C#/Home 34 Positiv 3 dig num/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = CreatArray(5, 100, 1000);
+
+int result = EvenNumber(array);
+
+PrintArray(array);
+Console.Write($" -> {result}");
+
+int[] CreatArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[arr.Length - 1]}]");
+    }
+
+
+}
+
+int EvenNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count += 1;
+    }
+    return count;
+}
