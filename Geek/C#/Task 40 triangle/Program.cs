5757a9b24d2769907f0524254ba28d98d25d21e0
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-16 14:15:57 +0300

    add task 40

---
+++ Geek/C#/Task 40 triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿//На вход три числа, могут ли быть трегольником? 
+//(одни сторона меньше суммы двух других)
+
+Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Triangle(a,b,c) ? "могут быть треугольникм" : "не могут быть треугольником");
+
+
+bool Triangle(int x,int y,int i)
+{
+    return x < y + i & y < x + i & i < x + y;
+}
+
+
+
