e2f68a526e845a513901dd985d4cc4c15870ced9
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-16 22:21:19 +0300

    add task42

---
+++ Geek/C#/task 42 Binary code/Program.cs
@@ -0,0 +1,58 @@
+﻿//Число в двоичный код записи
+
+
+int BinareCode(int num)
+
+{
+    int count = 0;
+    for (int i = 1; num != 0; i++)
+    {
+        num = num / 2;
+        count = i;
+
+    }
+    return count;
+}
+
+int[] Array(int len, int num)
+{
+    int[] arr = new int[len];
+
+    for (int i = 0; i != len; i++)
+    {
+
+        arr[len-1-i] = num % 2;
+        num = num / 2;
+    }
+    return arr;
+}
+
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]}");
+//     }
+
+// }
+
+Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int size = BinareCode(N);
+Console.WriteLine(size);
+int[] array = Array(size, N);
+
+// PrintArray(array);
+
+
+string result = string.Concat(array);
+int BinN = Convert.ToInt32(result);
+
+Console.WriteLine($"Число N в двоичной системе -> {BinN}");
+
+
+
+
+
