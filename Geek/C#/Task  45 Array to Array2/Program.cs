ce940e74311e7b0f67461d94318121b352e540e2
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-17 00:35:21 +0300

    add task 45

---
+++ Geek/C#/Task  45 Array to Array2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("оригинальный массив ->");
+int[] array = CreatArray(5, 0, 10);
+Printarray(array);
+
+Console.WriteLine("");
+
+int[] array2 = CopyArray(array);
+Console.WriteLine("репродукция массиав ->");
+Printarray(array2);
+
+
+
+
+int[] CreatArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+
+}
+
+void Printarray(int[] arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] arr2 = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr2[i] = arr[i];
+    }
+    return arr2;
+}
