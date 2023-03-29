b9fc347c2c9440187b96d5654d930eb63382cd5d
Bugaev Kirill <DurdenRD@gmail.com>
2023-03-30 01:14:11 +0300

    add task 60

--- Geek/C#/home 60/Program.cs
+++ Geek/C#/home 60/Program.cs
@@ -1,9 +1,48 @@
-﻿// Задача 60. ...Сформируйте трёхмерный массив 
-// из неповторяющихся двузначных чисел. 
-// Напишите программу, которая будет построчно выводить массив, 
+﻿// Задача 60. ...Сформируйте трёхмерный массив
+// из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив,
 // добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // Результат:
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
+int[,,] CraitMatrix(int rows, int colums, int dept, int min, int max)
+{
+    int[,,] matrix = new int[rows, colums, dept]; // 0, 1
+    int number = 10;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = number;
+                number++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] matr = CraitMatrix(2, 2, 2, 0, 10);
+PrintMatrix(matr);
+Console.WriteLine();
