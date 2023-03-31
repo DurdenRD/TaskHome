b477561efdf21f0cf5be218944646c668660f73d
Bugaev Kirill <DurdenRD@gmail.com>
2023-04-01 02:39:48 +0300

    add task 66

---
+++ Geek/C#/Task 66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Вывести сумму значения от M до N.
+
+
+Console.Write("Ведите M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"M = {M}; N = {N} -> {NaturNumber(M, N)} ");
+
+
+int  NaturNumber(int m, int n)
+{
+     if (m < n)
+    {
+      return  n + NaturNumber(m, n - 1);
+    }
+    else if (m > n)
+    {
+       return  m + NaturNumber(m - 1, n);
+        
+    }
+    else 
+    {
+        return n;
+    }
+}
--- Geek/C#/task 69/Program.cs
+++ Geek/C#/task 69/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на вход два числа A и B, A в степень целую B.
+
+Console.Write("Ведите A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите B = ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"A = {A}; B = {B} -> {Exponent(A, B)} ");
+
+
+int Exponent(int x, int y)
+{
+    if (y ==  0) return 1;
+    return x * Exponent(x, y-1);
+        
+
+    //SumDig(num / 10) % 10;
+}
