5089089db923b19345d3d750d6f1c306a0fa6ad4
Bugaev Kirill <DurdenRD@gmail.com>
2023-04-01 11:54:47 +0300

    add test integers home 66

--- Geek/C#/Task 66/Program.cs
+++ Geek/C#/Task 66/Program.cs
@@ -1,12 +1,18 @@
-﻿//Вывести сумму значения от M до N.
+﻿//Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
 
-Console.Write("Ведите M = ");
+Console.Write("Ведите натуральное число M = ");
 int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ведите N = ");
+Console.Write("Ведите натуральное число N = ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"M = {M}; N = {N} -> {NaturNumber(M, N)} ");
+if (M < 0 || N < 0) Console.WriteLine("Ввели не натуральное число(а)");
+else Console.Write($"M = {M}; N = {N} -> {NaturNumber(M, N)} ");
+
 
 
 int  NaturNumber(int m, int n)
